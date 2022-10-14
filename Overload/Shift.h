3944e6b36c996f2711070ac2020d297655fea2ae
Ekaterina Zhirnova <Katarinaetya4@gamil.com>
2022-10-14 21:24:54 +0300

    компилируется, но не компонуется, я не понимаю, в чем ошибка

---
+++ Overload/Shift.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "stdafx.h"
+#include"constants.h"
+
+template <typename T>
+void ShiftLeft(T arr[], const int n, int number_of_shifts);
+template <typename T>
+void ShiftLeft(T arr[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
+
+template <typename T>
+void ShiftRight(T arr[], const int n, int number_of_shifts);
